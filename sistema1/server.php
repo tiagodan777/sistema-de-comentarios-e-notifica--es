43d5a5d08b87f7b7c3a7d4009a5f79957978cd90
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-04-28 00:50:18 +0000

    aula 2 finalizada

--- sistema1/server.php
+++ sistema1/server.php
@@ -10,21 +10,39 @@ use Ratchet\ConnectionInterface;
 
 class NotificationsServer implements MessageComponentInterface {
     protected $clients;
+    private $pdo;
 
     public function __construct()
     {
         $this->clients = new \SplObjectStorage;
+
+        $this->pdo = new \PDO("mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;host=localhost;dbname=notificacoes", "root", "root");
     }
 
     public function onOpen(ConnectionInterface $conn)
     {
         $this->clients->attach($conn);
-        echo "Nova conexão! - ({$conn->resourceId})";
+        echo "Nova conexão! - ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
-        
+        $data = json_decode($msg, true);
+
+        $data['status'] = 0;
+
+        var_dump($data);
+
+        echo "YOU PARVA\n";
+
+        if (isset($data['type']) && $data['type'] === 'new_comment') {
+            $statement = $this->pdo->prepare("INSERT INTO comments (comment_subject, comment_text, comment_status) VALUES (:comment_subject, :comment_text)");
+            $statement->execute(['comment_subject' => $data['subject'], 'comment_text' => $data['comment'], 'comment_status' => 0]);
+            $this->broadcastNotifications();
+            echo "OKI DOKI PARVA\n";
+        } else {
+            echo "Sua parva\n";
+        }
     }
 
     public function onClose(ConnectionInterface $conn)
@@ -37,6 +55,12 @@ class NotificationsServer implements MessageComponentInterface {
     {
         echo "Ocorreu um erro: {$e->getMessage()}\n";
         $conn->close();
+    }
+
+    private function broadcastNotifications() {
+        foreach ($this->clients as $client) {
+            // Fazer a seguir
+        }
     }
 }
 
